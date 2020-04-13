ea4fb8e92fa453a30950c4a2b9cc6a78059dca1a
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-04-13 19:33:02 +0700

    update

--- index.js
+++ index.js
@@ -8,64 +8,97 @@
  */
 var fs = require("fs");
 var readline = require("readline-sync");
-var inputContact = require("./inputContact");
-var editContact = require("./editContact");
-var savetoJSON = require("./savetoJSON");
-var showContact = require("./showContact");
-var deleteContact = require("./deleteContact");
-var findContact = require("./findContact");
+
 var contacts = [];
 
 function loadData() {
   var file = fs.readFileSync("./data.json");
   contacts = JSON.parse(file);
 }
-function saveAndExit(data) {
-  savetoJSON(data);
+
+function inputContact() {
+  var name = readline.question("Name: ");
+  var phone = readline.question("Phone: ");
+  var input = {
+    name: name,
+    phone: phone,
+  };
+  contacts.push(input);
 }
+
+function editContact() {
+  showContact();
+  var choose = readline.question("Chọn vị trí muốn sửa: ");
+  choose = parseInt(choose) - 1;
+
+  var name = readline.question("Name: ");
+  var phone = readline.question("Phone: ");
+
+  contacts[choose].name = name;
+  contacts[choose].phone = phone;
+}
+
+function showContact() {
+  return contacts.map((index) =>
+    console.log("Name: " + index.name + " - Phone: " + index.phone)
+  );
+}
+
+function deleteContact() {
+  showContact();
+  var choose = readline.question("Chọn vị trí bạn muốn xoá: ");
+  choose = parseInt(choose) - 1;
+
+  contacts.splice(choose, 1);
+}
+
+function savetoJSON() {
+  var saveData = JSON.stringify(contacts);
+  fs.writeFileSync("./data.json", saveData, { encoding: "utf8" });
+}
+
+function saveAndExit() {
+  savetoJSON();
+}
+
 function option() {
-  console.log("1. Nhập dữ liệu contact");
-  console.log("2. Sửa dữ liệu contact");
-  console.log("3. Xoá contact");
-  console.log("4. Tìm kiếm contact");
-  console.log("5. Hiển thị contact");
-  console.log("0. Lưu và thoát");
+  console.log("0. Nhập dữ liệu contact");
+  console.log("1. Sửa dữ liệu contact");
+  console.log("2. Xoá contact");
+  console.log("3. Hiển thị contact");
+  console.log("4. Lưu và thoát");
   var select = readline.question("> ");
 
   switch (select) {
+    case "0":
+      inputContact();
+      savetoJSON();
+      option();
+      break;
     case "1":
-      inputContact(contacts);
-      savetoJSON(contacts);
+      editContact();
+      savetoJSON();
       option();
       break;
     case "2":
-      editContact(contacts);
-      savetoJSON(contacts);
+      deleteContact();
+      savetoJSON();
       option();
       break;
     case "3":
-      deleteContact(contacts);
-      savetoJSON(contacts);
+      showContact();
       option();
       break;
     case "4":
-      findContact(contacts);
-      option();
-      break;
-    case "5":
-      showContact(contacts);
-      option();
-      break;
-    case "0":
-      saveAndExit(contacts);
+      saveAndExit();
       break;
     default:
       console.log("Lỗi cú pháp");
       break;
   }
 }
-function home() {
+function main() {
   loadData();
   option();
 }
-home();
+main();
--- savetoJSON.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-
-function savetoJSON(data) {
-  var saveData = JSON.stringify(data);
-  fs.writeFileSync("./data.json", saveData, { encoding: "utf8" });
-}
-module.exports = savetoJSON;
